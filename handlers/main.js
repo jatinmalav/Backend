ce44359ab909e6a159bd509809ffbb6bfd255cf6
Jatin Malav <jatin.malav@saavn.com>
2023-09-29 23:14:57 +0530

    checking

--- handlers/main.js
+++ handlers/main.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
+
+
 app.listen(80, () => {
  console.log("Server running on port 3000");
 });
 
-app.get("/url", (req, res, next) => {
+app.get("/", (req, res, next) => {
     console.log("hello");
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
