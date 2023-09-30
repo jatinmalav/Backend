845d7a46bc0f47520e3862fdd060ca50ed50bf46
Jatin Malav <jatin.malav@saavn.com>
2023-09-30 19:58:45 +0530

    quick fixes

--- handlers/index.js
+++ handlers/index.js
@@ -6,6 +6,7 @@ app.listen(80, () => {
  console.log("Server running on port 3000");
 });
 
+
 app.get("/", (req, res, next) => {
     console.log("hello");
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
